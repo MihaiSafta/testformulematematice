19c1334fbfb03df645c4cef148471864c41eefdf
Mihai <mihaisaftas.259@gmail.com>
2019-07-03 21:23:52 +0300

    update cozalozawoza

---
+++ src/main/java/org/fasttrackit/AverageCalculator.java
@@ -0,0 +1,34 @@
+//package org.fasttrackit;
+//
+///**
+// Create a method in AverageCalculator to receive 3 numbers, display them and display their average.
+// 3. Invoke that method from AverageCalculator.main() with 3 numbers of your choice
+// */
+//public class AverageCalculator {
+//
+//    public void displayAverage(double a, double b, double c) {
+//        System.out.println("Numbers are :" + a + "," + b + "," + c);
+//        double average = (a + b + c) / 3;
+//        System.out.println("Average is :" + average);
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        AverageCalculator averageCalculator = new AverageCalculator();
+//        averageCalculator.displayAverage(2.5, 3, 5.5);
+//
+//        TemperatureCoverter temperatureCoverter = new TemperatureCoverter();
+//        temperatureCoverter.celfah(32.2);
+//
+//        TemperatureCoverter temperatureCoverter1 = new TemperatureCoverter();
+//        temperatureCoverter1.fahcel(90);
+//
+//        TemperatureCoverter met2 = new TemperatureCoverter();
+//        met2.sgmet(22.2, "Celsius");
+//    }
+//
+//
+//
+//
+//}
---
+++ src/main/java/org/fasttrackit/TemperatureCoverter.java
@@ -0,0 +1,53 @@
+//package org.fasttrackit;
+//
+//public class TemperatureCoverter {
+//    //A. Creati o metoda care sa primeasca o valoare numerica si sa transforme din Celsius in Fahrenheit
+//    //B. Creati o metoda care sa primeasca o valoare numerica(temp) si sa transforme din Fahrenheit in Celsius
+//    //C. apelati fiecare metoda din main cu dif. val.
+//    //metodele ar trebui sa afiseze valorile inainte si dupa conversie
+//
+//    // Var.2 creati o singura metoda care sa primeasca temperatura(numeric) si tipul de temperatura dorit (string)
+//    // si sa realizeze conversia necesara
+//    //apelati din main pentru fiecare caz
+//
+//    //formulele
+//    // C = (F - 32) * 5 /9
+//    // F = (C * 9 / 5) + 32
+//
+//    public static void main(String[] args) {
+//
+//    }
+//    public void celfah(double C) {
+//        double F = (C * 9 / 5) + 32;
+//        System.out.println("Temperature in C before conversion is:" + C);
+//        System.out.println("Temperature in F is :" + F);
+//    }
+//    public void fahcel(double F) {
+//        double C = (F - 32) * 5 /9;
+//        System.out.println("Temperature in F before conversion is " + F);
+//        System.out.println("Temp in C is : " + C);
+//
+//    }
+//
+//
+//     public void sgmet(double number, String name){
+//         //double C = (F - 32) * 5 /9;
+//        // F = (C * 9 / 5) + 32;
+//        double F;
+//        if (name.equals("Celsius")){
+//            number = (number * 9 / 5) + 32;
+//             System.out.println("Temp in F is: " + number);
+//
+//            else (name.equals("Fahrenheit"))
+//            {number = (number - 32) * 5 / 9;
+//                 System.out.println("Temp in C is: " + number);}
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
